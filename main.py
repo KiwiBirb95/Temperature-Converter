3c93b6af9038bc0c1913d304fd35f8bb26ee0ec0
Ryan Kwan <rkwan2@calstatela.edu>
2023-02-09 00:24:25 -0800

    Temperature Conversion

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def main():
+    print("Enter Minimum Temp")
+    min_temp = int(input())
+    print("Enter Max Temp")
+    max_temp = int(input())
+    temp_conversion(min_temp, max_temp)
+
+
+def temp_conversion(min_temp, max_temp):
+    for x in range(min_temp, max_temp + 1):
+        print("celsius=", x, "fahrenheit=", conversion_formula(x))
+
+
+def conversion_formula(cel):
+    fah = cel * (9.0 / 5.0) + 32
+    return fah
+
+
+if __name__ == "__main__":
+    sys.exit(main())
