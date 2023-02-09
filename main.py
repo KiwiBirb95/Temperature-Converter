74193351d1c721ac3395c426d8fd4d45db104799
Ryan Kwan <rkwan2@calstatela.edu>
2023-02-09 11:02:37 -0800

    Added Fahrenheit to Celsius Conversion

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ import sys
 
 
 def main():
-    print("Enter Minimum Temp")
+    print("Enter Minimum Temp:")
     min_temp = int(input())
-    print("Enter Max Temp")
+    print("Enter Max Temp:")
     max_temp = int(input())
     temp_conversion(min_temp, max_temp)
 
 
+# Converts Fahrenheit to Celsius
+# def temp_conversion(min_temp, max_temp):
+#     for x in range(min_temp, max_temp + 1):
+#         print("Celsius =", x, "Fahrenheit =", conversion_formula(x))
+
+
+# Converts Celsius to Fahrenheit
 def temp_conversion(min_temp, max_temp):
     for x in range(min_temp, max_temp + 1):
-        print("Celsius =", x, "Fahrenheit =", conversion_formula(x))
+        print("Fahrenheit =", x, "Celsius =", conversion_formula(x))
 
 
-def conversion_formula(cel):
-    fah = cel * (9.0 / 5.0) + 32
-    return fah
+# Displays Fahrenheit to Celsius
+# def conversion_formula(cel):
+#     fah = cel * (9.0 / 5.0) + 32
+#     return fah
 
+
+# Displays Celsius to Fahrenheit
+def conversion_formula(fah):
+    cel = fah * (5.0 / 9.0) - 32
+    return cel
 
 if __name__ == "__main__":
     sys.exit(main())
-
-# This is a test comment
-# This is annoying
